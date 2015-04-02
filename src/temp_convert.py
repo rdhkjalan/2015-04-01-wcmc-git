4f541357ba2da4de8191a2d8296ccca1081148c0
Radhika <rdhkjalan@gmail.com>
2015-04-02 10:09:45 -0400

    write f_to_C

--- src/temp_convert.py
+++ src/temp_convert.py
@@ -1,5 +1,10 @@
 def f_to_K(temp):
 	return ((temp - 32) * (5.0 / 9.0)) + 273.15
-def c_to_K(temp):
+def K_to_c(temp):
 	return (temp - 273)
+
+def f_to_c(temp):
+	temp_K = f_to_K(temp)
+	results = K_to_c(temp_K)
+	return(result)
 
